30df11ea47029c6fc3e74111e1f2ab5f3a936a49
jarmansingh <jarmansingh@lidolearning.com>
2022-03-30 23:12:57 +0530

    change the port to 8080

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || "8080";
 
 app.use(function(req, res, next) {
   res.locals.ua = req.get('User-Agent');
